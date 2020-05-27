7193f12cf4fde2883b9bb9306d84970fa33ff5af
Niranjan-Jyothi <niranjanjyothi8@gmail.com>
2020-05-27 22:29:58 +0530

    commit after adding collision detection between particles

--- canvas.js
+++ canvas.js
@@ -12,7 +12,32 @@ function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
 }
 let particles=[]
+function rotate(velocity,angle){
+  const rotatedvelocity = {
+    x : velocity.x*Math.cos(angle) - velocity.y*Math.sin(angle) ,
+    y : velocity.x*Math.sin(angle) + velocity.y*Math.cos(angle)
+  }
+  return rotatedvelocity;
+}
+function resolve(a,b){
+  const  xVelocityDiff= a.v.x - b.v.x;
+  const  yVelocityDiff= a.v.y - b.v.y;
+  const  xDist = b.x - a.x;
+  const  yDist = b.y - a.y;
+  if(xVelocityDiff*xDist+yVelocityDiff*yDist>=0){
+    const angle = -Math.atan2(b.y-a.y,b.x-a.x);
+    const ma=a.m; const mb=b.m;
+    const ua=rotate(a.v,angle);
+    const ub=rotate(b.v,angle);
+    const va={x: ua.x*(ma-mb)/(ma+mb)+ub.x*2*mb/(ma+mb) , y : ua.y}
+    const vb={x: ub.x*(ma-mb)/(ma+mb)+ua.x*2*ma/(ma+mb) , y : ub.y}
+    const vFinala=rotate(va,-angle);
+    const vFinalb=rotate(vb,-angle);
+    a.v.x=vFinala.x; a.v.y=vFinala.y;
+    b.v.x=vFinalb.x; b.v.y=vFinalb.y;
+  }
 
+}
 
 
 const canvas = document.querySelector('canvas')
@@ -48,8 +73,9 @@ class particle {
       x : (Math.random()-0.5)*2,
       y : (Math.random()-0.5)*2
     }
-
-      this.m = 2;
+    this.m = 2;
+       // this.m =randomIntFromRange(2,3);
+      // this.m = (Math.random()-0.5)*1;
     this.x = x
     this.y = y
     this.r = radius
@@ -66,6 +92,16 @@ class particle {
   }
 
   update(particles) {   this.draw();
+
+    for(let i=0;i<particles.length;i++){
+    if (this==particles[i]) {
+
+      continue; }
+    if ((distance(this.x,this.y,particles[i].x,particles[i].y)-this.r*2)<=0){
+
+       resolve(this,particles[i]);
+    }
+   }
 
      if (this.x-this.r<=0 || this.x+this.r>=innerWidth){
        this.v.x= -this.v.x;
@@ -85,8 +121,8 @@ class particle {
 function init() {
   particles = []
 console.log("i am at init start");
-  for (let i = 0; i < 300; i++) {
-    const r=15;
+  for (let i = 0; i < 150; i++) {
+    const r=20;
     let x=randomIntFromRange(r,canvas.width-r);
     let y=randomIntFromRange(r,canvas.height-r);
     let color = randomColor(colors);
